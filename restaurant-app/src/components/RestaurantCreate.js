54600f945efb919a434665a30df67623591b3e76
Dummy <neetugupta.jkctc@gmail.com>
2021-07-01 15:44:27 +0530

    add and show listing

--- restaurant-app/src/components/RestaurantCreate.js
+++ restaurant-app/src/components/RestaurantCreate.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantCreate extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name:null,
+            email:null,
+            rating:null,
+            address:null
+        }
+    }
+    create () {
+        fetch("http://localhost:3000/restaurant",{
+            method: "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(this.state)
+        }).then((result)=>{
+            result.json().then((resp)=>{
+                alert("Created Successfully!")
+            })
+        })
+    }
     render() {
         return (
             <div>
-                <h1>RestaurantCreate</h1>
+                <h1>Restaurant Create</h1>
+                <input type="text" placeholder="Enter Name" onChange={(event)=>this.setState({name:event.target.value})}/> <br></br> <br></br>
+                <input type="text" placeholder="Enter email" onChange={(event)=>this.setState({email:event.target.value})} /> <br></br> <br></br>
+                <input type="text" placeholder="Enter rating" onChange={(event)=>this.setState({rating:event.target.value})} /> <br></br> <br></br>
+                <input type="text" placeholder="Enter address" onChange={(event)=>this.setState({address:event.target.value})} /> <br></br> <br></br>
+                <input type="button" value="Click Me" onClick={()=>{this.create()}}/> <br></br> <br></br>
             </div>
         );
     }
--- restaurant-app/src/components/RestaurantList.js
+++ restaurant-app/src/components/RestaurantList.js
@@ -2,39 +2,53 @@ import React, { Component } from 'react';
 import {Table} from 'react-bootstrap'
 
 class RestaurantList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            list:null
+        }
+    }
+    componentDidMount () {
+        fetch("http://localhost:3000/restaurant").then((response)=>{
+            response.json().then((result)=>{
+                this.setState({list:result})
+            })
+        })
+    }
     render() {
         return (
             <div>
                 <h1>Restaurant List</h1>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td colSpan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
+                {this.state.list?
+                    <div>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Rating</th>
+                                    <th>Address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.list.map((item,i)=>
+                                    <tr>
+                                        <td>{item.id}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.rating}</td>
+                                        <td>{item.address}</td>
+                                    </tr>
+                                    )
+                                }                                
+                            </tbody>
+                        </Table>
+                    </div>
+                :
+                null
+                }
             </div>
         );
     }
