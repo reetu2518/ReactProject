0d6f6652d44a6ec0f6aca5be9cb6011bcc4b7d92
Dummy <neetugupta.jkctc@gmail.com>
2021-07-06 12:44:37 +0530

    search operation

--- restaurant-app/src/components/RestaurantSearch.js
+++ restaurant-app/src/components/RestaurantSearch.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 class RestaurantSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchData :null,
+            noData : false
+        }
+    }
+    search (key) {
+        fetch ('http://localhost:3000/restaurant?q='+key).then((result)=>{
+             result.json().then((resp)=>{
+                 if (resp.length>0) {
+                     this.setState({searchData:resp, noData:false})
+                 } else {
+                     this.setState({searchData:null, noData:true})
+                 }
+             })
+        })
+    }
     render() {
+        
         return (
             <div>
-                <h1>RestaurantSearch</h1>
+                <h1>Restaurant Search</h1>
+                <input type="text" onChange={(e)=>this.search(e.target.value)}/>
+                {
+                    <div>
+                        {
+                            this.state.searchData?
+                            <div>
+                                {
+                                    this.state.searchData.map((item)=><div>
+                                        <div className="search-row">{item.name}</div>
+                                        </div>)
+                                }
+                            </div>
+                            : ""
+                        }
+                        {
+                            this.state.noData? <h3>No data found</h3>:null
+                        }
+                    </div>
+                }
             </div>
         );
     }
